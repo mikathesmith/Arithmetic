1294e071a5c70690669dea42eb908f8b38c0a1b9
Mika Smith <mika.e.smith@gmail.com>
2017-09-08 15:46:13 +1200

    Scanning in input - got started with trying to convert LargeInt to String

--- Arithmetic.java
+++ Arithmetic.java
@@ -9,17 +9,83 @@ import java.io.*;
 
 public class Arithmetic{
 	
+	
+	
 	public static void main(String[]args){
-		
-	}
-	
-	
-	private class largeInt{
-		
-		public largeInt(){
+		Scanner sc = new Scanner(System.in); //scan in a file 
+		while(sc.hasNextLine()){
+			String s = sc.nextLine(); 
+			char op = 'c'; 
+			if(!s.contains("#")){ //Ignore lines with # comments
+				Scanner sc2 = new Scanner(s);
+        		LargeInt x = new LargeInt(sc2.nextInt()); 
+        		op = sc2.next().charAt(0); 
+        		LargeInt y = new LargeInt(sc2.nextInt());
+        		System.out.println(calculate(x, y, op));
+        	}
+		}
+		sc.close(); 
 			
 		}
+	
+	public static String calculate(LargeInt x, LargeInt y, char op){
+		String result="";
+		result+=ConvertString(x); 
+		result+= op;
+		result+=ConvertString(y); 
+		
+		return result; 
 	}
+		
+		//ignore #
+		//each line new scenario. 
+		//read in operation and two numbers, can be more than one operation and more than two numbers  
+		
+		//print at the end 
+		
+		
+	
+	
+	//multiplication method 
+	
+	//division method 
+	
+	//GCD method 
+	
+	
+	
+	
+	//Our new data structure 
+	private static class LargeInt{
+		private int value; 
+		
+		
+		public LargeInt(){
+		}
+		
+		public LargeInt(int value){
+			this.value = value; 
+		}
+		
+		
+		public String ConvertString(LargeInt l){
+			return Integer.toString(l.value);
+		}
+		/*
+		
+		public largeInt subtract(largeInt x, largeInt y){
+			
+		}
+		
+		public largeInt half(largeInt x, largeInt y){
+			
+		}
+		
+		public largeInt compare(largeInt x, largeInt y){
+			
+		}*/ 
+	}
+	
 	
 	
 }
